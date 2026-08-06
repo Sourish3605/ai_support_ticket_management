f26520cb017c92c84c6221433ca74eb5988b8342
Premalatha <premalathatanniru2006@gmail.com>
2026-08-07 00:45:45 +0530

    Prepare backend for Render deployment

--- server/apps/staff/models.py
+++ server/apps/staff/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("Agent", "Agent"),
+        ("Customer", "Customer"),
+    ]
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="Customer"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
