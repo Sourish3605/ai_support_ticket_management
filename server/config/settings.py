257e04c7b5249edd6a2e436a40c433609fe0df88
Sourish3605 <sourishnarendrula@gmail.com>
2026-08-12 18:11:46 +0530

    Connect frontend to Render backend and fix local config

--- server/config/settings.py
+++ server/config/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4173',
+    'http://127.0.0.1:4173',
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
